1b32c17f0ca496132406c3a789959deb80088a19
JohaoMalfatti <johaomarcelo@gmail.com>
2022-07-31 19:27:07 -0300

    laços

--- main.py
+++ main.py
@@ -1,36 +1,9 @@
 from msilib.schema import Class
 
 
-var_1 = 10
-var_2 = 8
-soma = var_1+var_2
- 
-
-
-var_3 = "johao"
-var_4 = "malfatti"
-
-print(str(soma) + var_3 + var_4)
-
-######################################################
-class calc:
-
-    def Somar(var1, var2): 
-        return print(var1 + var2)  
-
-    
-    def Subtrair(var1, var2): 
-        return print(var1 - var2)  
-
-
-    def Multiplicar(var1, var2): 
-        return print(var1 * var2)  
-
-
-    def Dividir(var1, var2): 
-         return print(var1 / var2)  
-
-calc.Somar(4,5)
-calc.Subtrair(4,2)
-calc.Multiplicar(3,3)
-calc.Dividir(4,2)
+a = 5
+b = 6 
+if a > b:
+    print("A variável marior é a")
+if a < b:
+    print("A variável maior é a b")
